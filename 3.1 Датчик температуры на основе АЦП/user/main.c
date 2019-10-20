284237538d37680da524819aad14b5afb1275ca1
alpolyakov <56022044+alpolyakov@users.noreply.github.com>
2019-10-20 11:32:31 +0300

    Update main.c

--- 3.1 Датчик температуры на основе АЦП/user/main.c
+++ 3.1 Датчик температуры на основе АЦП/user/main.c
@@ -6,6 +6,8 @@
 #include "MDR32F9Qx_rst_clk.h"
 #include "MDR32F9Qx_adc.h"              // Keil::Drivers:ADC
 #include "MDR32F9Qx_port.h"
+#include "lcd.h"
+#include "mlt_lcd.h"
 
 #define ADC_TS_T1 25.0F // ??????????? ? 1-? ?????
 #define ADC_TS_D1 0x6A0 // ???????? ??? ? 1-? ?????
@@ -20,6 +22,8 @@ int res=0,temp=0,i;
 
 int main(void)
 {
+	//U_RST_Init();	
+	U_MLT_Init(); //Инициализация дисплея
 	RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA|
 	RST_CLK_PCLK_PORTB|RST_CLK_PCLK_PORTC|
 	RST_CLK_PCLK_PORTD|RST_CLK_PCLK_ADC, ENABLE); 
@@ -29,12 +33,12 @@ int main(void)
 	MDR_RST_CLK->ADC_MCO_CLOCK=0x2000;	 
 	
 	// Set up pins for LEDs
-	PORT_InitStructure.PORT_Pin=(PORT_Pin_0 | PORT_Pin_1); 
+	/*PORT_InitStructure.PORT_Pin=(PORT_Pin_0 | PORT_Pin_1); 
 	PORT_InitStructure.PORT_OE = PORT_OE_OUT; 
 	PORT_InitStructure.PORT_FUNC = PORT_FUNC_PORT;
 	PORT_InitStructure.PORT_MODE = PORT_MODE_DIGITAL; 
 	PORT_InitStructure.PORT_SPEED = PORT_SPEED_SLOW; 
-	PORT_Init(MDR_PORTC, &PORT_InitStructure);
+	PORT_Init(MDR_PORTC, &PORT_InitStructure);*/
 	
 	// Setting output for ADC
 	PORT_InitStructure.PORT_Pin = PORT_Pin_7;
@@ -45,11 +49,11 @@ int main(void)
 	// ADC setup
 	ADC_InitStructure.ADC_SynchronousMode = ADC_SyncMode_Independent; // Select Independent Mode
 	ADC_InitStructure.ADC_StartDelay = 0;                             // Zero delay when starting the conversion
-	ADC_InitStructure.ADC_TempSensor = ADC_TEMP_SENSOR_Enable;       // Turn off 
-	ADC_InitStructure.ADC_TempSensorAmplifier = ADC_TEMP_SENSOR_AMPLIFIER_Enable; // Turn off the amplifier of the temperature sensor
-  ADC_InitStructure.ADC_TempSensorConversion = ADC_TEMP_SENSOR_CONVERSION_Enable;  // Turn off temperature sensor conversion
-	ADC_InitStructure.ADC_IntVRefConversion = ADC_VREF_CONVERSION_Enable;  // Turn off preobrazovaniye opornogo napr
-  ADC_InitStructure.ADC_IntVRefTrimming = 1;                        // Coefficient for voltage reference (opornoe napryajenie)
+	ADC_InitStructure.ADC_TempSensor = ADC_TEMP_SENSOR_Enable;       // Turn on 
+	ADC_InitStructure.ADC_TempSensorAmplifier = ADC_TEMP_SENSOR_AMPLIFIER_Enable; // Turn on the amplifier of the temperature sensor
+ 	ADC_InitStructure.ADC_TempSensorConversion = ADC_TEMP_SENSOR_CONVERSION_Enable;  // Turn on temperature sensor conversion
+	ADC_InitStructure.ADC_IntVRefConversion = ADC_VREF_CONVERSION_Enable;  // Turn on preobrazovaniye opornogo napr
+ 	ADC_InitStructure.ADC_IntVRefTrimming = 1;                        // Coefficient for voltage reference (opornoe napryajenie)
 	ADC_Init(&ADC_InitStructure);
 	
 	ADCx_InitStructure.ADC_ClockSource = ADC_CLOCK_SOURCE_CPU; //Select a clock source
@@ -70,18 +74,8 @@ int main(void)
 	ADC1_Cmd (ENABLE);  // Start ADC
 	
 	while(1)
-	{
-		if (temp > 34) 
-		{
-			PORT_SetBits(MDR_PORTC, PORT_Pin_1);
-			PORT_ResetBits(MDR_PORTC, PORT_Pin_0);
-		}
-		else
-		{			
-			PORT_ResetBits(MDR_PORTC, PORT_Pin_1);
-			PORT_SetBits(MDR_PORTC, PORT_Pin_0);
-		}
-	 	for (i=0;i<5000;i++); 
+	{		
+		
 	}
 }
 
@@ -89,7 +83,15 @@ void ADC_IRQHandler(void)
 {
 	ADC1_Cmd(DISABLE);              //Stop the ADC
 	res=ADC1_GetResult()&0x0FFF;            //Read the conversion result
-	temp = ((int32_t)res - (int32_t)ADC_TS_D1)*(ADC_TS_T2-ADC_TS_T1)/(ADC_TS_D2 - ADC_TS_D1)+ ADC_TS_T1;
+	
+	// Перевод в Цельсии
+	temp = ((int32_t)res - (int32_t)ADC_TS_D1)*(ADC_TS_T2-ADC_TS_T1)/(ADC_TS_D2 - ADC_TS_D1)+ ADC_TS_T1; 
+	
+	 //Вывод на дисплей
+	U_LCD_Task_Function(temp);
+	
 	NVIC_ClearPendingIRQ(ADC_IRQn);  //Ochistka bita ozhidaniya vneshnego preryvaniya EXTI1
 	ADC1_Cmd(ENABLE);               //Start ADC
+	for (i=0;i<500000;i++) {};
 }
+
